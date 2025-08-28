d47fbc0e9305391f3c89be0959c2cae080e52bea
nikitinas <as.nikitin@promenergo-rt.ru>
2025-08-28 16:50:55 +0300

    1234567891112

--- libs/parsing.py
+++ libs/parsing.py
@@ -3,6 +3,12 @@ from datetime import datetime
 
 from gurux_dlms import GXUInt8, GXUInt16, GXStructure, GXUInt32, GXDateTime
 from gurux_dlms.enums import DataType
+from gurux_dlms.objects import GXDLMSHdlcSetup, GXDLMSCaptureObject, GXDLMSData, GXDLMSDisconnectControl, \
+    GXDLMSRegister, GXDLMSAutoConnect
+
+
+# from libs.GXDateTime import GXDateTime
+
 
 def parse_data_object_for_write(obj, reader, value, attribute):
     if obj.logicalName in ['0.0.128.1.0.255', '0.0.128.2.0.255']:
@@ -86,7 +92,140 @@ def parse_clock_object_for_write(obj, reader, value, attribute):
             else:
                 raise Exception('Только True или False!!!')
         else:
-            raise Exception('Данный атрибут недоступен для записи!!!')
+            raise Exception('Атрибут не поддерживает запись!!!')
         return obj
     except Exception as e:
         raise
+
+
+# def get_all_spodes_for_display(reader):
+#     device_type = reader.deviceType
+#     if device_type == '1PH':
+#         return "1.0.1.8.0.255 1.0.1.8.1.255 1.0.1.8.2.255 1.0.1.8.3.255 1.0.1.8.4.255 " \
+#                "1.0.2.8.0.255 1.0.2.8.1.255 1.0.2.8.2.255 1.0.2.8.3.255 1.0.2.8.4.255 " \
+#                "1.0.3.8.0.255 1.0.3.8.1.255 1.0.3.8.2.255 1.0.3.8.3.255 1.0.3.8.4.255 " \
+#                "1.0.4.8.0.255 1.0.4.8.1.255 1.0.4.8.2.255 1.0.4.8.3.255 1.0.4.8.4.255 " \
+#                "1.0.12.7.0.255 1.0.11.7.0.255 1.0.91.7.0.255 1.0.1.7.0.255 1.0.3.7.0.255 " \
+#                "1.0.9.7.0.255 1.0.13.7.0.255 1.0.14.7.0.255 1.0.1.8.0.101 " \
+#                "1.0.1.8.1.101 1.0.1.8.2.101 1.0.1.8.3.101 1.0.1.8.4.101 1.0.2.8.0.101 " \
+#                "1.0.2.8.1.101 1.0.2.8.2.101 1.0.2.8.3.101 1.0.2.8.4.101 1.0.3.8.0.101 " \
+#                "1.0.3.8.1.101 1.0.3.8.2.101 1.0.3.8.3.101 1.0.3.8.4.101 1.0.4.8.0.101 " \
+#                "1.0.4.8.1.101 1.0.4.8.2.101 1.0.4.8.3.101 1.0.4.8.4.101 0.0.22.0.0.255 " \
+#                "0.1.22.0.0.255 0.2.22.0.0.255 0.0.96.3.10.255 0.0.96.1.2.255 0.0.96.1.8.255 " \
+#                "0.0.96.4.3.255 0.0.96.1.0.255 0.0.0.9.1.255 0.0.0.9.2.255"
+#     elif device_type == '3PH':
+#         return "1.0.1.8.0.255 1.0.1.8.1.255 1.0.1.8.2.255 1.0.1.8.3.255 1.0.1.8.4.255 " \
+#                "1.0.2.8.0.255 1.0.2.8.1.255 1.0.2.8.2.255 1.0.2.8.3.255 1.0.2.8.4.255 " \
+#                "1.0.3.8.0.255 1.0.3.8.1.255 1.0.3.8.2.255 1.0.3.8.3.255 1.0.3.8.4.255 " \
+#                "1.0.4.8.0.255 1.0.4.8.1.255 1.0.4.8.2.255 1.0.4.8.3.255 1.0.4.8.4.255 " \
+#                "1.0.32.7.0.255 1.0.52.7.0.255 1.0.72.7.0.255 1.0.124.7.0.255 1.0.125.7.0.255 " \
+#                "1.0.126.7.0.255 1.0.91.7.0.255 1.0.31.7.0.255 1.0.51.7.0.255 1.0.71.7.0.255 " \
+#                "1.0.1.7.0.255 1.0.21.7.0.255 1.0.41.7.0.255 1.0.61.7.0.255 1.0.3.7.0.255 " \
+#                "1.0.23.7.0.255 1.0.43.7.0.255 1.0.63.7.0.255 1.0.9.7.0.255 1.0.29.7.0.255 " \
+#                "1.0.49.7.0.255 1.0.69.7.0.255 1.0.13.7.0.255 1.0.33.7.0.255 1.0.53.7.0.255 " \
+#                "1.0.73.7.0.255 1.0.14.7.0.255 1.0.1.8.0.101 1.0.1.8.1.101 " \
+#                "1.0.1.8.2.101 1.0.1.8.3.101 1.0.1.8.4.101 1.0.2.8.0.101 1.0.2.8.1.101 " \
+#                "1.0.2.8.2.101 1.0.2.8.3.101 1.0.2.8.4.101 1.0.3.8.0.101 1.0.3.8.1.101 " \
+#                "1.0.3.8.2.101 1.0.3.8.3.101 1.0.3.8.4.101 1.0.4.8.0.101 1.0.4.8.1.101 " \
+#                "1.0.4.8.2.101 1.0.4.8.3.101 1.0.4.8.4.101 0.0.22.0.0.255 0.1.22.0.0.255 " \
+#                "0.2.22.0.0.255 0.0.96.3.10.255 0.0.96.1.2.255 0.0.96.1.8.255 0.0.96.4.3.255 " \
+#                "0.0.96.1.0.255 0.0.0.9.1.255 0.0.0.9.2.255"
+#     else:
+#         return "1.0.1.8.0.255 1.0.1.8.1.255 1.0.1.8.2.255 1.0.1.8.3.255 1.0.1.8.4.255 " \
+#                "1.0.2.8.0.255 1.0.2.8.1.255 1.0.2.8.2.255 1.0.2.8.3.255 1.0.2.8.4.255 " \
+#                "1.0.3.8.0.255 1.0.3.8.1.255 1.0.3.8.2.255 1.0.3.8.3.255 1.0.3.8.4.255 " \
+#                "1.0.4.8.0.255 1.0.4.8.1.255 1.0.4.8.2.255 1.0.4.8.3.255 1.0.4.8.4.255 " \
+#                "1.0.32.7.0.255 1.0.52.7.0.255 1.0.72.7.0.255 1.0.124.7.0.255 1.0.125.7.0.255 " \
+#                "1.0.126.7.0.255 1.0.91.7.0.255 1.0.31.7.0.255 1.0.51.7.0.255 1.0.71.7.0.255 " \
+#                "1.0.1.7.0.255 1.0.21.7.0.255 1.0.41.7.0.255 1.0.61.7.0.255 1.0.3.7.0.255 " \
+#                "1.0.23.7.0.255 1.0.43.7.0.255 1.0.63.7.0.255 1.0.9.7.0.255 1.0.29.7.0.255 " \
+#                "1.0.49.7.0.255 1.0.69.7.0.255 1.0.13.7.0.255 1.0.33.7.0.255 1.0.53.7.0.255 " \
+#                "1.0.73.7.0.255 1.0.14.7.0.255 1.0.1.8.0.101 1.0.1.8.1.101 " \
+#                "1.0.1.8.2.101 1.0.1.8.3.101 1.0.1.8.4.101 1.0.2.8.0.101 1.0.2.8.1.101 " \
+#                "1.0.2.8.2.101 1.0.2.8.3.101 1.0.2.8.4.101 1.0.3.8.0.101 1.0.3.8.1.101 " \
+#                "1.0.3.8.2.101 1.0.3.8.3.101 1.0.3.8.4.101 1.0.4.8.0.101 1.0.4.8.1.101 " \
+#                "1.0.4.8.2.101 1.0.4.8.3.101 1.0.4.8.4.101 0.0.22.0.0.255 0.1.22.0.0.255 " \
+#                "0.2.22.0.0.255 0.0.96.1.2.255 0.0.96.1.8.255 " \
+#                "0.0.96.1.0.255 0.0.0.9.1.255 0.0.0.9.2.255"
+
+
+def load_display_readout(all_values):
+    capture_objects = []
+    for obis in all_values:
+        # if index == number_of_elements:
+        #     break
+        # if obis in old_capture_objects_names:
+        #     continue
+
+        if obis in "0.0.22.0.0.255 0.1.22.0.0.255 0.2.22.0.0.255":
+            obj = GXDLMSHdlcSetup(obis)
+            capture_object = GXDLMSCaptureObject(2, 0)
+            item = (obj, capture_object)
+            capture_objects.append(item)
+            continue
+        elif obis in "0.0.96.1.2.255 0.0.96.1.8.255 0.0.96.4.3.255 0.0.96.1.0.255 0.0.0.9.1.255 0.0.0.9.2.255":
+            obj = GXDLMSData(obis)
+            capture_object = GXDLMSCaptureObject(2, 0)
+            item = (obj, capture_object)
+            capture_objects.append(item)
+            continue
+        elif obis == "0.0.96.3.10.255":
+            obj = GXDLMSDisconnectControl(obis)
+            capture_object = GXDLMSCaptureObject(3, 0)
+            item = (obj, capture_object)
+            capture_objects.append(item)
+            continue
+        elif obis not in "0.2.22.0.0.255":
+            obj = GXDLMSRegister(obis)
+            capture_object = GXDLMSCaptureObject(2, 0)
+            item = (obj, capture_object)
+            capture_objects.append(item)
+            continue
+    return capture_objects
+
+
+def parse_profile_generic_object_for_write(obj, reader, value, attribute):
+    if obj.logicalName == '0.0.21.0.1.255':
+        if attribute == '4':
+            obj.capturePeriod = int(value)
+        elif attribute == '3':
+            value = re.sub(r'[^0-9,.]', '', value)
+            value = value.split(',')
+            capture_objects = load_display_readout(value)
+            obj.captureObjects = capture_objects
+    elif attribute == '4':
+        obj.capturePeriod = int(value)
+    else:
+        raise Exception('Атрибут не поддерживает запись!!!')
+    return obj
+
+
+def parse_auto_connect_object_for_write(obj, reader, value, attribute):
+    if attribute == '2' and value in ['101', '104']:
+        obj.mode = value
+    elif attribute == '3':
+        obj.repetitions = int(value)
+    elif attribute == '4':
+        obj.repetitionDelay = int(value)
+    elif attribute == '5':
+        value = re.sub(r'[^0-9,.:* ]', '', value)
+        value = value.split(',')
+        value[0] = value[0].strip()
+        print(value[0])
+        value[1] = value[1].strip()
+        print(value[1])
+
+        start = GXDateTime(value[0], '%m.%d.%Y %H:%M:%S')
+        print(start.value)
+        end = GXDateTime(value[1], '%m.%d.%Y %H:%M:%S')
+        print(end.value)
+        obj.callingWindow.append((start, end))  #  'NoneType' object has no attribute 'seconds' ???
+
+        print(obj.callingWindow)
+    elif attribute == '6':
+        value = re.sub(r'[^0-9,.:]', '', value)
+        value = value.split(',')
+        obj.destinations = value
+    else:
+        raise Exception('Атрибут не поддерживает запись!!!')
+    return obj
--- libs/recorded_objects.py
+++ libs/recorded_objects.py
@@ -1,10 +1,11 @@
 import re
 from datetime import datetime
 
-from gurux_dlms import GXUInt8, GXUInt16, GXStructure, GXUInt32
+from gurux_dlms import GXUInt8, GXUInt16, GXStructure, GXUInt32, GXDateTime
 from gurux_dlms.enums import DataType, ObjectType
 
-from libs.parsing import parse_data_object_for_write, parse_clock_object_for_write
+from libs.parsing import parse_data_object_for_write, parse_clock_object_for_write, \
+    parse_profile_generic_object_for_write, parse_auto_connect_object_for_write
 
 
 def set_value(obj, reader, value, attribute):
@@ -14,6 +15,12 @@ def set_value(obj, reader, value, attribute):
         return set_value_for_register(obj, reader, value, attribute)
     elif obj.getObjectType() == ObjectType.CLOCK:
         return set_value_for_clock(obj, reader, value, attribute)
+    elif obj.getObjectType() == ObjectType.PROFILE_GENERIC:
+        return set_value_for_profile_generic(obj, reader, value, attribute)
+    elif obj.getObjectType() == ObjectType.GPRS_SETUP:
+        return set_value_for_gprs_setup(obj, reader, value, attribute)
+    elif obj.getObjectType() == ObjectType.AUTO_CONNECT:
+        return set_value_for_auto_connect(obj, reader, value, attribute)
     else:
         raise NotImplementedError('Пока этот класс не обрабатывается!!!')
 
@@ -47,8 +54,6 @@ def set_value_for_clock(obj, reader, value, attribute):
     try:
         data_type = reader.readType(obj, int(attribute))
         obj = parse_clock_object_for_write(obj, reader, value, attribute)
-        # scalar = reader.read(obj, 3)[0]
-        # obj.value = int(int(value) / scalar)
         obj.setDataType(int(attribute), data_type)
         reader.write(obj, int(attribute))
         return True
@@ -57,3 +62,40 @@ def set_value_for_clock(obj, reader, value, attribute):
         return False
 
 
+def set_value_for_profile_generic(obj, reader, value, attribute):
+    try:
+        data_type = reader.readType(obj, int(attribute))
+        obj = parse_profile_generic_object_for_write(obj, reader, value, attribute)
+        obj.setDataType(int(attribute), data_type)
+        reader.write(obj, int(attribute))
+        return True
+    except Exception as e:
+        print(f"Ошибка при записи атрибута {attribute} объекта {obj.logicalName} класса Profile Generic >>", e)
+        return False
+
+
+def set_value_for_gprs_setup(obj, reader, value, attribute):
+    try:
+        data_type = reader.readType(obj, int(attribute))
+        if attribute == '2':
+            obj.apn = value
+        else:
+            raise Exception('Атрибут не поддерживает запись!!!')
+        obj.setDataType(int(attribute), data_type)
+        reader.write(obj, int(attribute))
+        return True
+    except Exception as e:
+        print(f"Ошибка при записи атрибута {attribute} объекта {obj.logicalName} класса GPRSSetup >>", e)
+        return False
+
+
+def set_value_for_auto_connect(obj, reader, value, attribute):
+    try:
+        data_type = reader.readType(obj, int(attribute))
+        obj = parse_auto_connect_object_for_write(obj, reader, value, attribute)
+        # obj.setDataType(int(attribute), data_type)
+        reader.write(obj, int(attribute))
+        return True
+    except Exception as e:
+        print(f"Ошибка при записи атрибута {attribute} объекта {obj.logicalName} класса AutoConnect >>", e)
+        return False
